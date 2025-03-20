fec68f5cb603b2f8ee850f403b64899a8ba5b2a8
Thecodingpanda <th3codingpanda@gmail.com>
2025-03-20 11:52:05 +0100

    Update camera almost got it working

--- Assets/Camera_Swap.cs
+++ Assets/Camera_Swap.cs
@@ -5,7 +5,7 @@ public class Camera_Swap : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D _polygonCollider2D;
     [SerializeField] CameraScript _cameraScript;
-    private Camera[] _camera;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,14 +20,7 @@ public class Camera_Swap : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) {
-            Debug.Log(collision.transform.parent.name);
-         
-            
-            _camera = collision.transform.parent.GetComponentsInChildren<Camera>();
-            Debug.Log(_camera[0].name);
-            
             _cameraScript.Change(_polygonCollider2D);
-            Debug.Log(_camera[0].name);
         }
     }
 }
--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField]Camera _camera;
+    [SerializeField]Transform _transform;
     private PolygonCollider2D _polygonCollider;
     void Start()
     {
-        
+        _camera.transform.position = new Vector3(_transform.position.x, _transform.position.y , _camera.transform.position.z);
     }
     // Update is called once per frame
     void Update()
-    {
-        if(_camera.transform.position.y)
-        _camera.transform.position = new Vector3(_camera.transform.position.x, _camera.transform.position.y, _camera.transform.position.z);
+    {   // edit math clamp to take in camera size
+        _camera.transform.position = new Vector3(Mathf.Clamp(_transform.position.x + _camera.orthographicSize/2, _polygonCollider.points[0].x, _polygonCollider.points[2].x), _transform.position.y, -1);
         if (Input.GetMouseButton(0)) {
             Debug.Log(_camera.transform.position);
             //do camera stuff with points max out -15 prob because size wow wowoowowowowowowowo
@@ -28,7 +28,7 @@ public class CameraScript : MonoBehaviour
     }
     public void Change(PolygonCollider2D polygonCollider2D) {
         _polygonCollider = polygonCollider2D;
-        Debug.Log(_polygonCollider.points[0].y);
-
+        Debug.Log(_polygonCollider.points[0].y); 
+        
     }
 }
